6a6c9fdee03b5d81b8e08a0d32dd72fc36739e17
99hi <mt310k@gmail.com>
2020-11-02 20:29:29 +0900

    テスト

---
+++ pages/auth/social-callback.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <p>少しお待ちください...</p>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      token: this.$route.query.token ? this.$route.query.token : null
+    }
+  },
+  mounted() {
+    this.$auth.login({
+      data: {
+        token: this.token
+      }
+    }).then(res => {
+      console.log("成功")
+      console.log(res)
+    }).catch(e => {
+      console.log("エラー")
+      console.log(e)
+      //return this.$router.push('/error')
+    })
+  }
+}
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  ログインテスト
+  <v-btn @click="login">LINEログイン</v-btn>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    async register() {
+      try {
+        await this.axios.post('/auth/register', this.form)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    login() {
+      axios.get('/api/login/line').then(res => {
+        console.log(res.data)
+      })
+    },
+
+  }
+
+}
+</script>
